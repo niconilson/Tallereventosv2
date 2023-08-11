0c8e884974612d47eed6f6e482358636c7974e85
niconilson <niconilson@hotmail.com>
2023-08-10 21:54:44 -0300

    Versión 2

---
+++ eventos.js
@@ -0,0 +1,31 @@
+/*let btn = document.querySelector("#paso8");
+
+btn.addEventListener("click", function() {
+  alert('Hola! Soy el div');
+});
+*/
+
+/*div.addEventListener("click", () => {
+    alert('Hola! Soy el div');
+  });
+  
+
+  function saludodiv() {
+    alert('Hola! Soy el div')
+  }
+  */
+
+  // paso8.addEventListener("click", alert('Hola! Soy el div'))
+
+/* document.getElementById("paso8").addEventListener("click", funcion());
+
+function funcion() {
+  alert('Hola! Soy el div');
+}
+*/
+
+click = document.getElementById("btndiv");
+
+click.addEventListener("click", function () {
+  alert('Hola! Soy el div')
+})
